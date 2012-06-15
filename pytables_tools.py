a90098584eec9a3884b0257d3705f0af1ad1dfe8
Merlijn van Deen <valhallasw+meetpc116@gmail.com>
2012-06-15 14:18:00 +0200

    Bugfix for fa51e04: particles-> data

--- pytables_tools.py
+++ pytables_tools.py
@@ -22,7 +22,7 @@ def require_table(root, name, dtype):
         return root._v_file.createTable(root, name, dtype)
 
 def store_table(root, name, data, *args, **kwargs):
-    return root._v_file.createTable(root, name, data, expectedrows=data.shape[0], chunkshape=particles.shape)
+    return root._v_file.createTable(root, name, data, expectedrows=data.shape[0], chunkshape=data.shape)
 
 def add_to_table(table, data={}, **kwargs):
     data = data.copy()
