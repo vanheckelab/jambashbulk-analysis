9579da284a86ab8a8771df93ac4b49e59af8e52f
Merlijn van Deen <valhallasw+novamaris@gmail.com>
2012-10-16 16:23:26 +0200

    'Handle' errors by logging them to stderr and ignoring them

--- packing_tools/calc_moduli.py
+++ packing_tools/calc_moduli.py
@@ -8,15 +8,19 @@ if len(sys.argv) < 2:
 
 t = tables.File(sys.argv[1])
 for group in iter(t.root._v_groups.items()[0][1]).next():
-    packing = dict((x, group._v_attrs[x]) for x in group._v_attrs._v_attrnames)
-    packing['particles'] = group.particles.read()
-    
-    contacts = V_harm.get_contacts(packing)
+    try:
+        packing = dict((x, group._v_attrs[x]) for x in group._v_attrs._v_attrnames)
+        packing['particles'] = group.particles.read()
+        
+        contacts = V_harm.get_contacts(packing)
 
-    info = {'N': packing['N'], 'P0': packing['P0'], 'PackingNumber': int(packing['PackingNumber'])}
-    info.update(V_harm.V_harm(contacts, packing))
-    info.update(V_harm.grad(contacts, packing))
-    K = V_harm.Hess(contacts, packing)
+        info = {'N': packing['N'], 'P0': packing['P0'], 'PackingNumber': int(packing['PackingNumber'])}
+        info.update(V_harm.V_harm(contacts, packing))
+        info.update(V_harm.grad(contacts, packing))
+        K = V_harm.Hess(contacts, packing)
 
-    info.update(V_harm.El_Con(K, contacts['rattlers'], packing))
-    print repr(info).replace("\n", "\t")
+        info.update(V_harm.El_Con(K, contacts['rattlers'], packing))
+        print repr(info).replace("\n", "\t")
+    except Exception, e:
+        import traceback
+        traceback.print_exc(file=sys.stderr)
