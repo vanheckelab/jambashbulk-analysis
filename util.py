4efef14e30f868740854e33854687d727bdece5c
Merlijn van Deen <valhallasw+isilwen@gmail.com>
2014-03-26 14:54:49 +0100

    Simple mounted volume mapper

--- util.py
+++ util.py
@@ -4,6 +4,7 @@ Created on Thu Jun 13 11:35:00 2013
 
 @author: Merlijn van Deen
 """
+import os
 from pylab import *
 
 def NPmapper(N,P, colormap='Accent'):
@@ -23,3 +24,19 @@ def NPmapper(N,P, colormap='Accent'):
     color = cmap(normedP)
     
     return {'marker': marker, 'color': color}
+    
+def getmappeddrive(substr):
+    drives = {}
+    for lineset in os.popen("mountvol").read().split("\n\n"):
+        if lineset.strip().startswith('\\\\?\\Volume'):
+            vol, path = lineset.split("\n")
+            vol = vol.strip()
+            path = path.strip()
+            if path.startswith("***"):
+                continue
+            if substr in vol:
+                return path
+    raise Exception('drive not found')
+    
+ESPCIDrive = lambda: getmappeddrive("{07c40d6f-c442-11e1-b08d-9cb70dd0f78e}")
+ToshibaUSB3 = lambda: getmappeddrive("{a2449b94-7082-11e2-a88c-028037ec0200}")
